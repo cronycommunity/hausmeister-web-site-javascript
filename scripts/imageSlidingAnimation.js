e240b138b1c95eb3a108109b7c7387dc1d1763ad
azizkale <azizkale@hotmail.com>
2022-09-08 18:09:45 +0200

    fix-animation

--- scripts/imageSlidingAnimation.js
+++ scripts/imageSlidingAnimation.js
@@ -1,51 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const image1 = document.getElementById("img1");
-    const image2 = document.getElementById("img2");
-    const image3 = document.getElementById("img3");
-    image1.style.left = "-1200px";
-    image2.style.left = "-1200px";
-    image3.style.left = "-1200px";
+    let position = 0;
+    let animationRepetingControl = 0;
+    window.addEventListener("scroll", function(e) {
+        imageAnimations();
+        position = window.scrollY;
+    });
 
-});
-let position = 0;
-let animationRepetingControl = 0;
-window.addEventListener("scroll", function(e) {
-    imageAnimations();
-    position = window.scrollY;
-    console.log(position)
-});
+    const imageAnimations = () => {
+        if (position > 10 && animationRepetingControl == 0) {
+            document.getElementById("img1").animate([
+                // keyframes
+                { transform: 'translateX(-1200px)' },
+                { transform: 'translateX(0px)' }
+            ], {
+                // timing options
+                duration: 1500,
+                iterations: 1
+            });
+            animationRepetingControl++;
 
-const imageAnimations = () => {
+        } else if (position > 400 && animationRepetingControl == 1) {
+            document.getElementById("img2").animate([
+                // keyframes
+                { transform: 'translateX(-1200px)' },
+                { transform: 'translateX(0px)' }
+            ], {
+                // timing options
+                duration: 1500,
+                iterations: 1
+            });
+            animationRepetingControl++;
 
-    const imageSliding = [
-        { left: "-500px" },
-        { left: "0px" }
-    ];
-
-    const imageTiming = {
-        duration: 1000,
-        iterations: 1,
+        } else if (position > 800 && animationRepetingControl == 2) {
+            document.getElementById("img3").animate([
+                // keyframes
+                { transform: 'translateX(-1200px)' },
+                { transform: 'translateX(0px)' }
+            ], {
+                // timing options
+                duration: 1500,
+                iterations: 1
+            });
+            animationRepetingControl++;
+        }
     }
-
-    if (position > 10 && animationRepetingControl == 0) {
-        const image = document.getElementById("img1");
-        image.animate(imageSliding, imageTiming);
-        document.getElementById("img1").style.left = "0px";
-        animationRepetingControl++;
-
-    } else if (position > 400 && animationRepetingControl == 1) {
-        const image = document.getElementById("img2");
-        image.animate(imageSliding, imageTiming);
-        document.getElementById("img2").style.left = "0px";
-        animationRepetingControl++;
-
-    } else if (position > 800 && animationRepetingControl == 2) {
-        const image = document.getElementById("img3");
-        image.animate(imageSliding, imageTiming);
-        document.getElementById("img3").style.left = "0px";
-        animationRepetingControl++;
-    }
-
-
-
-}
