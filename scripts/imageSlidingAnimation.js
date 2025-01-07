c5fb0d9accb3702f11db196a3aa1405aad19185a
Mete Demirtas <mete.demirtas@emm-it.de>
2025-01-07 21:30:47 +0100

    animations are deleted

--- scripts/imageSlidingAnimation.js
+++ scripts/imageSlidingAnimation.js
@@ -7,27 +7,27 @@
 
     ////////////////////////////
 
-//    let position = 0;
-//    let animationRepetingControl = 0;
-//    window.addEventListener("scroll", function(e) {
-//        imageAnimations();
-//        position = window.scrollY;
-//    });
-//
-//    const imageAnimations = () => {
-//        if (position > 10 && animationRepetingControl == 0) {
-//            img1.style.animation = "slidingAnimation 0.1s linear 1";
-//            img1.style.transform = "translateX(" + (0) + "%)";
-//            animationRepetingControl++;
-//
-//        } else if (position > 400 && animationRepetingControl == 1) {
-//            img2.style.animation = "slidingAnimation 0.1s linear 1";
-//            img2.style.transform = "translateX(" + (0) + "%)";
-//            animationRepetingControl++;
-//
-//        } else if (position > 800 && animationRepetingControl == 2) {
-//            img3.style.animation = "slidingAnimation 0.1s linear 1";
-//            img3.style.transform = "translateX(" + (0) + "%)";
-//            animationRepetingControl++;
-//        }
-//    }
+    let position = 0;
+    let animationRepetingControl = 0;
+    window.addEventListener("scroll", function(e) {
+        imageAnimations();
+        position = window.scrollY;
+    });
+
+    const imageAnimations = () => {
+        if (position > 10 && animationRepetingControl == 0) {
+            img1.style.animation = "slidingAnimation 0.1s linear 1";
+            img1.style.transform = "translateX(" + (0) + "%)";
+            animationRepetingControl++;
+
+        } else if (position > 400 && animationRepetingControl == 1) {
+            img2.style.animation = "slidingAnimation 0.1s linear 1";
+            img2.style.transform = "translateX(" + (0) + "%)";
+            animationRepetingControl++;
+
+        } else if (position > 800 && animationRepetingControl == 2) {
+            img3.style.animation = "slidingAnimation 0.1s linear 1";
+            img3.style.transform = "translateX(" + (0) + "%)";
+            animationRepetingControl++;
+        }
+    }
